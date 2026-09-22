c2759316c0275bb1da5acc39ab2422e1f1aa7f4c
Mathew Lipson <mathew.lipson@bom.gov.au>
2026-09-22 15:50:55 +1000

    new script for n96 urban comparison

---
+++ notebooks/sandbox-python/run_compare_urban_onoff_year.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -q normalsr
+#PBS -l ncpus=16
+#PBS -l mem=80GB
+#PBS -l walltime=02:00:00
+#PBS -l storage=gdata/xp65+scratch/gb02+scratch/ce10
+#PBS -l wd
+#PBS -l jobfs=40GB
+#PBS -P ce10
+
+module purge
+module use /g/data/xp65/public/modules
+module load conda/analysis3-25.08
+
+python /home/561/mjl561/git/access-am3-evaluation/notebooks/sandbox-python/compare_urban_onoff_year.py
